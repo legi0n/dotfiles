6b988386edae7ddf18464b9ad60047be6e1398f4
legi0n <76619180+legi0n@users.noreply.github.com>
2026-02-26 15:02:58 +0200

    feat: add initial nvim and some more stuff

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,8 @@
+-------------
+-- Options --
+-------------
+
+-- See :help vim.opt and :help option-list
+
+vim.opt.number = true         -- Show absolute line number
+vim.opt.relativenumber = true -- Show relative line number

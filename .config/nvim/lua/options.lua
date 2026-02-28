5cf51d670343c785d1922df9b8b187b10240c1d5
legi0n <me@alexandrujoita.com>
2026-02-28 20:29:22 +0200

    modified:   .config/nvim/lua/options.lua

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,3 +6,5 @@
 
 vim.opt.number = true         -- Show absolute line number
 vim.opt.relativenumber = true -- Show relative line number
+
+vim.opt.shiftwidth = 4 -- Number of spaces for indentation

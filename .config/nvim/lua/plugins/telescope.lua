c2de10fc52b82a76edcccc1e68fbfd5b3b8d161b
legi0n <76619180+legi0n@users.noreply.github.com>
2026-03-13 19:40:47 +0200

    feat(nvim): check git repo before calling telescope git_files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,7 +7,17 @@ return {
     },
     keys = {
         { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-        { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find Git Files" },
+        {
+            "<leader>fg",
+            function()
+                if vim.fn.system("git rev-parse --is-inside-work-tree 2>/dev/null") == "true\n" then
+                    require("telescope.builtin").git_files()
+                else
+                    print("Not inside a Git repository")
+                end
+            end,
+            desc = "Find Git Files",
+        },
         { "<leader>fl", "<cmd>Telescope live_grep<cr>", desc = "Find with Grep" },
         { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find Buffers" },
     },
